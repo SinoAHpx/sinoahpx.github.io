211fe5f56db34ad293115ec584553fbfc2ec8c09
破晓 <AHpx@yandex.com>
2026-04-07 16:44:41 +0800

    Configure blog.ahpx.me for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahpx.me',
+  site: 'https://blog.ahpx.me',
   integrations: [mdx(), sitemap()],
 
   vite: {
